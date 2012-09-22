d783bdc30536e5abf8bf7cff0a5ef45d7f39b53f
Kyle Fritz <kyle.p.fritz@gmail.com>
2012-09-21 23:27:07 -0400

    time in degrib

--- degrib.rb
+++ degrib.rb
@@ -1,20 +1,21 @@
 require 'csv'
+require 'date'
 
 class Degrib
   def self.read(metric,point)
     file_path = File.expand_path(File.join('data', "ds.#{metric}.bin"))
     raise "can't file file at #{file_path}" unless File.exists?(file_path)
-    CSV.parse(`degrib #{file_path} -P -pnt #{point}`)
+    rows = CSV.parse(`degrib #{file_path} -P -pnt #{point}`)[1..-1]
+
+    rows.sort_by! { |row| (DateTime.now - DateTime.parse(row[-2])).magnitude }
+    rows[0][-1].to_f
   end
 
   def self.wind_direction(point)
-    vals = self.read('wdir', point)
-    #TODO: take time into account
-    vals[1][-1].to_f
+    self.read('wdir', point)
   end
+
   def self.wind_speed(point)
-    vals = self.read('wspd', point)
-    #TODO: take time into account
-    vals[1][-1].to_f
+    self.read('wspd', point)
   end
 end
