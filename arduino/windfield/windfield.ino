a3cb5bf70f631936b3f21ca1d6beb4a24096304e
Kyle Fritz <kyle.p.fritz@gmail.com>
2012-09-22 00:12:59 -0400

    cleanup arduino code

--- arduino/windfield/windfield.ino
+++ arduino/windfield/windfield.ino
@@ -1,66 +1,32 @@
 #include <Servo.h>
 
+const int FANS              = 4;
+const int FAN_1ST_RELAY_PIN = 3;
+const int FAN_1ST_SERVO_PIN = 9;
+const int ZERO_CROSS_PIN    = 2;
+const int SERVO_0_DEG       = 40;
+const int SERVO_360_DEG     = 140;
+
 struct Fan{
-  int relay; //which relay pin
+  int relay;
   Servo servo;
 };
 
-const int FANS = 4;
-const int FAN_1ST_RELAY_PIN=3;
-const int FAN_1ST_SERVO_PIN=9;
-const int ZERO_CROSS_PIN=2;
-
 Fan fans[FANS];
 
-const int SERVO_0_DEG= 40;
-const int SERVO_360_DEG = 140;
-
-//fan state
-int angle = 0; // a-s (0-18)
-int speed = 0; //a-s (0-18)
+//state
+int angle = 0;
+int speed = 0;
+int nthCross = 0;
 
 /*
+a-s (0-18)
 a   0 0
-b  20 1
-c  40 2
 d  60 3
-e  80 4
-f 100 5
-g 120 6
-h 140 7
-i 160 8
 j 180 9
-k 200 10
-l 220 11
-m 240 12
 n 260 13
-o 280 14
-p 300 15
-q 320 16
-r 340 17
 s 360 18
 */
-
-void setup(){
-  Serial.begin(9600);
-
-  //servo
-  for(int i=0; i<FANS; i++){
-    fans[i].relay = i + FAN_1ST_RELAY_PIN;
-    pinMode(fans[i].relay, OUTPUT);
-    fans[i].servo.attach(i + FAN_1ST_SERVO_PIN);
-  }
-  angleFans();
-  
-  pinMode(ZERO_CROSS_PIN, INPUT); 
-  attachInterrupt(0, onZeroCross, CHANGE);
-  
-  pinMode(13, OUTPUT);
-  digitalWrite(13,LOW);
-  blink(500);
-  blink(500);
-  blink(500);
-}
 
 void blink(int ms)
 {
@@ -71,26 +37,8 @@ void blink(int ms)
   digitalWrite(13,LOW);
 }
 
-void loop() {}
-
-void serialEvent() {
-  
-  int _;
-  while (Serial.available() < 2) { _++; }
-  
-  angle = 20*((int)Serial.read() - 'a');
-  speed = (int)Serial.read() - 'a';
-  
-  Serial.print("angle ");
-  Serial.print(angle);
-  Serial.print(" speed ");
-  Serial.println(speed); 
-  
-  angleFans();
-}
-
 void angleFans(){
-  int servoPosition = constrain((angle/3.6)+SERVO_0_DEG,SERVO_0_DEG,SERVO_360_DEG);  
+  int servoPosition = constrain((angle/3.6)+SERVO_0_DEG,SERVO_0_DEG,SERVO_360_DEG);
    for(int i=0; i<FANS; i++){
     fans[i].servo.write(servoPosition);
   }
@@ -101,12 +49,49 @@ void switchFans(int mode){
    }
 }
 
-int nthCross;
+void setup(){
+  Serial.begin(9600);
+
+  for(int i=0; i<FANS; i++){
+    fans[i].relay = i + FAN_1ST_RELAY_PIN;
+    pinMode(fans[i].relay, OUTPUT);
+    fans[i].servo.attach(i + FAN_1ST_SERVO_PIN);
+  }
+  angleFans();
+
+  pinMode(ZERO_CROSS_PIN, INPUT);
+  attachInterrupt(0, onZeroCross, CHANGE);
+
+  pinMode(13, OUTPUT);
+  digitalWrite(13,LOW);
+  blink(500);
+  blink(500);
+  blink(500);
+}
+
+void loop() {}
+
+void serialEvent() {
+
+  int _;
+  while (Serial.available() < 2) { _++; }
+
+  angle = 20*((int)Serial.read() - 'a');
+  speed = (int)Serial.read() - 'a';
+
+  Serial.print("angle ");
+  Serial.print(angle);
+  Serial.print(" speed ");
+  Serial.println(speed);
+
+  angleFans();
+}
+
 void onZeroCross()
 {
   nthCross++;
 //  digitalWrite(13, (nthCross % 2) ?HIGH:LOW);
-  
+
   if(speed <= 0){
     switchFans(LOW);
   }
@@ -120,9 +105,8 @@ void onZeroCross()
     int mod = nthCross % nthWave;
     switchFans((mod==0||mod==1)?HIGH:LOW);
   }
-  else
+  else //if(4 < speed <= 17)
   {
-    //if(4 < speed <= 17)
     int dimTime = 7000 - 500*(speed-4); //(7k-1k by 500)
     switchFans(LOW);
     delayMicroseconds(dimTime);
